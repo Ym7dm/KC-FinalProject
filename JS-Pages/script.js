7dccad35f0ec255d34b0d73448cc932e92ace303
Yahya Mohamed <109866996+Ym7dm@users.noreply.github.com>
2022-09-07 23:46:22 +0300

    Done

--- JS-Pages/script.js
+++ JS-Pages/script.js
@@ -1,4 +1,9 @@
-function navbar() {
-  let navbar = document.getElementById("navbar");
-  navbar.style.background - color("#333333");
-}
+let icon = document.getElementById("icon");
+icon.onclick = function dark() {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "../Images/sun.png";
+  } else {
+    icon.src = "../Images/moon.png";
+  }
+};
